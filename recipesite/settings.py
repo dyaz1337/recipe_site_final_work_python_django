37c32c6b792c4cc65d27804776cfdb605a04ac84
dyaz1337 <112968308+dyaz1337@users.noreply.github.com>
2024-04-04 03:30:03 +0500

    Update settings.py

--- recipesite/settings.py
+++ recipesite/settings.py
@@ -139,9 +139,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
