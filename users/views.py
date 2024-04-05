379515e7aeb6bc978fc3dcd94cc3c7db66d0d9ce
dyaz1337 <112968308+dyaz1337@users.noreply.github.com>
2024-04-05 20:37:48 +0500

    Update views.py

--- users/views.py
+++ users/views.py
@@ -16,9 +16,6 @@ class LoginUser(LoginView):
     template_name = 'users/login.html'
     extra_context = {'title': 'Авторизация'}
 
-    # def get_success_url(self):
-    #     return reverse_lazy('home')
-
 
 class RegisterUser(CreateView):
     form_class = RegisterUserForm
